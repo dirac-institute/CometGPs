56636c5ff0e894ff6a8a7b6f75485a17cf43d4d2
Christina Lindberg <christina.lindbeg@live.com>
2018-11-12 12:57:41 -0800

    added subsampling methods

--- cwl_sandbox/subsample.py
+++ cwl_sandbox/subsample.py
@@ -24,11 +24,19 @@ def subsample(time, flux, flux_err=None, npoints=100, kind="random",
         There are different sampling patterns currently implemented:
             "random" : randomly sample `npoints` data points from
                        `time` and `flux1`
-            "telescope": take day/night cycle into account, but randomly
+            "telescope" : take day/night cycle into account, but randomly
                          subsample within a "night"
             "sparse" : sparse asteroid sampling: 3 x 1-minute images, separated by 21-minute intervals,
                                                  whole night, for K nights in a row and N nights per semester
-            "ztf" :
+            "dense" : dense asteroid sampling: observations every 5 minutes for 8 hours every nights
+
+            "semi-dense" : 15 observations per night every 30 minutes on average over 8 hours,
+                            then 3 nights later, than skipping a month
+            "talc" : 17 observations per night every 15 minutes on average over 3-4 hours,
+                            for 5 nights over a 2 week periods
+            "ztf/lsst" : 2 observations per night separated by 60 minutes over 3 nights
+                            not sequential (e.g. 1, 5, 8 (4+/-1))
+
 
     time_unit : str
         The unit of time that the `time` array is in.
@@ -157,6 +165,7 @@ def subsample(time, flux, flux_err=None, npoints=100, kind="random",
             elif time_unit == "hours":
                 tstart += (24.0 - night_length)
                 tend += 24.0
+
     elif kind == "ztf":
 
         tstart = time[0]
@@ -198,6 +207,220 @@ def subsample(time, flux, flux_err=None, npoints=100, kind="random",
                 ferrsmall.append(flux_err[t_ind])
             else:
                 ferrsmall.append(sigma)
+
+    elif kind == "dense":
+        if time_unit == "days":
+            night_length /= 24.0
+            tseg = time[-1] - time[0] # total length of the time series
+        else:
+            tseg = (time[-1] - time[0]) / 24.0
+
+        tstart = time[0]
+        tend = tstart + night_length
+
+        tsmall = []
+        fsmall = []
+
+        ferrsmall = []
+
+        # one minute in fractions of days
+        dt = 1./(60*24.)
+
+
+        while tend <= time[-1]:
+
+            #within the 8-hour night
+            while tstart <= tend:
+                tind = time.searchsorted(tstart)
+                tsmall.append(time[tind])
+                fsmall.append(flux[tind])
+
+                if flux_err is not None:
+                    ferrsmall.append(flux_err[tind])
+                else:
+                    ferrsmall.append(sigma)
+
+                tstart += 5.*dt
+
+            if time_unit == "days":
+                tstart += (1.0-night_length)
+                tend += 1.0
+            elif time_unit == "hours":
+                tstart += (24.0 - night_length)
+                tend += 24.0
+
+    elif kind == "semi-dense":
+        if time_unit == "days":
+            night_length /= 24.0
+            tseg = time[-1] - time[0] # total length of the time series
+        else:
+            tseg = (time[-1] - time[0]) / 24.0
+
+        tstart = time[0]
+        tend = tstart + night_length
+
+        tsmall = []
+        fsmall = []
+
+        ferrsmall = []
+
+        # one minute in fractions of days
+        dt = 1./(60*24.)
+
+        skip_month = False
+        while tend <= time[-1]:
+
+            #within 15 obeservations
+            nobs = 0
+            while nobs < 15:
+                #generate 15 observations per night
+                total_time = 999
+                while total_time > 8.0:
+                    start_id = np.random.normal(30, 10, size=15)
+                    total_time = start_id.sum()/60.
+
+                for j in range(15):
+                    tind = time.searchsorted(tstart)
+                    tsmall.append(time[tind])
+                    fsmall.append(flux[tind])
+
+                    if flux_err is not None:
+                        ferrsmall.append(flux_err[tind])
+                    else:
+                        ferrsmall.append(sigma)
+
+                    tstart += start_id[j]*dt
+
+                    nobs+=1
+
+                night_length = start_id.sum()/60.
+
+            if skip_month==False:
+                if time_unit == "days":
+                    tstart += (3.0-night_length/24.)
+                    tend += 3.0
+
+                elif time_unit == "hours":
+                    tstart += (72.0 - night_length)
+                    tend += 72.0
+
+            else:
+                if time_unit == "days":
+                    tstart += (26.0-night_length/24.)
+                    tend += 26.0
+
+                elif time_unit == "hours":
+                    tstart += (24.0*26. - night_length)
+                    tend += 24.0*26.
+
+            skip_month = not skip_month
+
+    elif kind == "talc":
+        if time_unit == "days":
+            night_length /= 24.0
+            tseg = time[-1] - time[0] # total length of the time series
+        else:
+            tseg = (time[-1] - time[0]) / 24.0
+
+        tstart = time[0]
+        tend = tstart + night_length
+
+        tsmall = []
+        fsmall = []
+
+        ferrsmall = []
+
+        # one minute in fractions of days
+        dt = 1./(60*24.)
+
+        days_total = 999
+        while days_total > 14:
+            day_jump= np.random.randint(1,6, size=4)
+            days_total = day_jump.sum()
+            day_jump = np.append(day_jump, [0])
+
+        day_obs = 0
+        while day_obs < 5:
+            #within 17 obeservations
+            nobs = 0
+            while nobs < 17:
+                #generate 17 observations per night
+                start_id = np.random.normal(15, 3, size=17)
+
+                for j in range(17):
+                    tind = time.searchsorted(tstart)
+                    tsmall.append(time[tind])
+                    fsmall.append(flux[tind])
+
+                    if flux_err is not None:
+                        ferrsmall.append(flux_err[tind])
+                    else:
+                        ferrsmall.append(sigma)
+
+                    tstart += start_id[j]*dt
+
+                    nobs+=1
+
+                night_length = start_id.sum()/60.
+
+            if time_unit == "days":
+                tstart += (1.0*day_jump[day_obs]-night_length/24.)
+                tend += 1.0
+
+            elif time_unit == "hours":
+                tstart += (24.0 - night_length)
+                tend += 24.0
+
+            day_obs+=1
+
+    elif kind == "ztf/lsst":
+        if time_unit == "days":
+            night_length /= 24.0
+            tseg = time[-1] - time[0] # total length of the time series
+        else:
+            tseg = (time[-1] - time[0]) / 24.0
+
+        tstart = time[0]
+        tend = tstart + night_length
+
+        tsmall = []
+        fsmall = []
+
+        ferrsmall = []
+
+        # one minute in fractions of days
+        dt = 1./(60*24.)
+
+        while tend <= time[-1]:
+            nobs=0
+            night_length=0
+
+            #within 2 observations
+            while nobs <= 1:
+                tind = time.searchsorted(tstart)
+                tsmall.append(time[tind])
+                fsmall.append(flux[tind])
+
+                if flux_err is not None:
+                    ferrsmall.append(flux_err[tind])
+                else:
+                    ferrsmall.append(sigma)
+
+                time_jump = np.random.normal(60,5)
+                night_length += time_jump
+
+                tstart += time_jump*dt
+
+                nobs+=1
+
+            day_jump = np.random.randint(3,6)
+
+            if time_unit == "days":
+                tstart += (1.0*day_jump-night_length/(24.*60.))
+                tend += 1.0*day_jump
+            elif time_unit == "hours":
+                tstart += (24.0*day_jump - night_length/(60.))
+                tend += 24.0*day_jump
 
     fsmall = np.random.normal(np.hstack(fsmall), scale=np.hstack(ferrsmall)) + mean_flux
 
