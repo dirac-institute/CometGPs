b485e97ae2dd0c63deac871293fad4f72684b4f5
christinawlindberg <christina.lindberg@live.com>
2019-06-07 10:42:48 -0700

    new print statements

--- cwl_sandbox/rejection_sampler.py
+++ cwl_sandbox/rejection_sampler.py
@@ -80,41 +80,29 @@ def main():
     L_results = []
     # L_results = pool.map(manager.calculate_likelihood, [i for i in np.arange(nsamples)])
     L_results = pool.map(manager.calculate_likelihood, zip(J_mean, J_log_amp, np.exp(J_log_gamma), J_log_period))
+
+    print("Pool has finished.")
+
     L_results = np.array(L_results)
+
+    print("Results have been converted into an array!")
 
     pool.close()
 
     end_time = tm.time()
     print("\nTotal time taken : %.2f seconds" %(end_time - start_time))
 
-    #L_results = np.ones(nsamples)
-
-    #for i in np.arange(nsamples):
-        # input the new sampled period value into the kernel
-#        params = [mean_flux, log_amp, gamma, J[i]]
-#        gp.set_parameter_vector(params)
-
-        # calculate the log likelihood
-        # and check to see if it is a valid number
-#        try:
-#            gp.compute(time, flux_err)
-#            lnlike = gp.log_likelihood(flux)
-#        except np.linalg.LinAlgError:
-#            lnlike = -1e25
-
-        # add log likelihood value to results array
-#        L_results[i] = lnlike
-
     # 3 : Pick a random number r out of a uniform distribution between 0 and 1
     #     and compare the normalized (between 0 and 1) likelihood value to it
 
 
-    uu = rnd.uniform(size=len(L_results))
 
-    good_samples_bool = uu < np.exp(L_results-L_results.max())
-    good_samples_idx, = np.where(good_samples_bool)
 
-    print("Number of samples passed : %s" %len(good_samples_idx))
+    good_samples_bool = rnd.uniform(size=len(L_results)) < np.exp(L_results-L_results.max())
+    #good_samples_idx, = np.where(good_samples_bool)
+
+    #summing the bool = passes
+    print("Number of samples passed : %s" %sum(good_samples_bool))
 
     # print(np.exp(J[good_samples_idx])*24.)
 
@@ -125,7 +113,7 @@ def main():
 
     now = datetime.datetime.now().strftime("%Y-%m-%d-%Hh%Mm")
 
-    data = np.array([np.exp(J_log_period[good_samples_idx])*24, np.exp(J_log_gamma[good_samples_idx]), np.exp(J_log_amp[good_samples_idx]), J_mean[good_samples_idx], L_results[good_samples_idx]]).T
+    data = np.array([np.exp(J_log_period[good_samples_bool])*24, np.exp(J_log_gamma[good_samples_bool]), np.exp(J_log_amp[good_samples_bool]), J_mean[good_samples_bool], L_results[good_samples_bool]]).T
     print(data)
 
     np.savetxt(filename + "results_%s.txt" %(now), data)
@@ -145,7 +133,7 @@ def main():
             "processors: %d \n" % processors +
             "time: %.2f sec \n" % float(end_time - start_time) +
             "uu: %.2f \n" %uu +
-            "passed: %d \n\n" %len(good_samples_idx) +
+            "passed: %d \n\n" %sum(good_samples_bool) +
             str(data))
     f.close()
 
